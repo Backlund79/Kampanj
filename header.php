fa8022cd9d770e463a655901c5959f737f7f595a
Emil Backlund <emilbacklund@outlook.com>
2020-10-23 13:39:34 +0200

    Menu function, ACF hero tags

--- header.php
+++ header.php
@@ -9,3 +9,12 @@
 <body <?php body_class(); ?>>
   Head
   <hr>
+  <?php
+  if (have_rows('acf_hero')) {
+      while (have_rows('acf_hero')) {
+          the_row();
+          if (get_row_layout() == 'acf_hero_section') {
+              get_template_part('template-parts/hero');
+          }
+      }
+  }
---
+++ template-parts/hero.php
@@ -0,0 +1,8 @@
+<?php
+
+//ACF field taggs.
+// hero_img
+// hero_title
+// hero_text
+// cta_knapp_text
+// cta_link
