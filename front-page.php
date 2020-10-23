4c8614831ca9050991c1ad9afbe2b5cab995ac52
Emil Backlund <emilbacklund@outlook.com>
2020-10-23 14:04:42 +0200

    ACF spaces setup

--- front-page.php
+++ front-page.php
@@ -1,3 +1,13 @@
 <?php get_header(); ?>
 frontpage
+<?php
+if (have_rows('acf_spacer')) {
+    while (have_rows('acf_spacer')) {
+        the_row();
+        if (get_row_layout() == 'acf_spacer_section') {
+            get_template_part('template-parts/spacer');
+        }
+    }
+}
+?>
 <?php get_footer();
---
+++ template-parts/spacer.php
@@ -0,0 +1,4 @@
+<?php
+// rubrik
+// farg
+// textfalt
