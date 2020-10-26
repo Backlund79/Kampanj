b79101d1425293dd59f5490882bcdb2b157c6c83
Emil Backlund <emilbacklund@outlook.com>
2020-10-26 15:10:05 +0100

    break needed

--- template-parts/center-img.php
+++ template-parts/center-img.php
@@ -4,3 +4,11 @@
 // image
 // cta_button_text
 // cta_link
+?>
+
+<section>
+  <div class="center-image"
+    style="background-image: url('<?php the_sub_field('image'); ?>')">
+
+  </div>
+</section>
