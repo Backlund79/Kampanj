ecc3439032cf30d0b32c5a42a8d7c93fe319e34e
Emil Backlund <emilbacklund@outlook.com>
2020-10-26 14:19:52 +0100

    Left block done

--- template-parts/left-block.php
+++ template-parts/left-block.php
@@ -5,3 +5,16 @@
 // text
 // color
 // text_color
+?>
+
+<section>
+  <div
+    style="background-image: url('<?php the_sub_field('image'); ?>')"
+    class="col-md-6 left-block vcenter-item text-center">
+    <div class="left-block-text"
+      style="color:<?php the_sub_field('text_color'); ?>">
+      <?php the_sub_field('text') ?>
+    </div>
+
+  </div>
+</section>
