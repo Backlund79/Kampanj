327bb403f220eab229979918b8fb7ef5acc07941
Emil Backlund <emilbacklund@outlook.com>
2020-10-26 15:06:21 +0100

    Right block done

--- template-parts/left-block.php
+++ template-parts/left-block.php
@@ -7,14 +7,12 @@
 // text_color
 ?>
 
-<section>
+<div class="row ">
   <div
-    style="background-image: url('<?php the_sub_field('image'); ?>')"
-    class="col-md-6 left-block vcenter-item text-center">
+    style="background-image: url('<?php the_sub_field('image'); ?>'); background-color: <?php the_sub_field('color'); ?>)"
+    class="col-md-6 left-block text-center d-flex align-items-center">
     <div class="left-block-text"
       style="color:<?php the_sub_field('text_color'); ?>">
       <?php the_sub_field('text') ?>
     </div>
-
   </div>
-</section>
--- template-parts/right-block.php
+++ template-parts/right-block.php
@@ -5,3 +5,15 @@
 // text
 // color
 // text_color
+?>
+
+<div
+  style="background-image: url('<?php the_sub_field('image'); ?>'); background-color: <?php the_sub_field('color'); ?>;"
+  class="col-md-6 left-block center">
+  <div class="left-block-text"
+    style="color:<?php the_sub_field('text_color'); ?>">
+    <?php the_sub_field('text') ?>
+  </div>
+
+</div>
+</div>
